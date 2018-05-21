49141277be591100c3f4f151fe4f47f7ed8ec49e
BRENTON FORD <brenton.ford@synerty.com>
2018-05-21 23:39:16 +0200

    implement new game button

--- app.js
+++ app.js
@@ -11,17 +11,32 @@ GAME RULES:
 
 var scores, roundScore, activePlayer, dice;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+init();
+
+function init() {
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
 
 
-document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    
+    document.querySelector('#name-0').textContent = 'Player1';
+    document.querySelector('#name-1').textContent = 'Player2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
+
+
+document.querySelector('.btn-new').addEventListener('click',init);
 
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
